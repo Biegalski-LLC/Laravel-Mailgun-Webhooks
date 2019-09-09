6e55dc807885c1be24a605bfbe995988a32e12a8
Michael Biegalski <michael@biegalski-llc.com>
2019-09-08 23:20:08 -0400

    Composer fix and route update

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,18 @@
 <?php
 
+/**
+ * Mailgun Webhooks Middleware Route Group
+ */
 Route::group(['middleware' => ['mailgun_webhooks']], function () {
+
+    /**
+     * Prefixed and Named Route Group
+     */
     Route::prefix('api/mg-webhooks')->name('mailgun.')->group(function () {
+
+        /**
+         * Route
+         */
         Route::post('{type}', '\Biegalski\LaravelMailgunWebhooks\Http\Controllers\MailgunWebhooksController@messageType')->name('messageType');
     });
 });
