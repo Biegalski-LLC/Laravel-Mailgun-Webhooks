ae3abd8e320ab99f61ce11552c0c9d22892e43a3
Przemek <przemek.jaskulski@gmail.com>
2020-03-07 00:39:18 +0100

    Polish translation

---
+++ src/Resources/lang/pl/email.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'greeting' => 'Cześć,',
+    'clicks' => [
+        'subject' => 'Link kliknięty',
+        'desc' => 'Odbiorca kliknął na link w Twojej wiadomości, poniżej dane dotyczące tej wiadomości:'
+    ],
+    'delivered' => [
+        'subject' => 'Wiadomość dostarczona',
+        'desc' => 'Wiadomość została pomyślnie dostarczona do odbiorcy, poniżej dane dotyczące tej wiadomości:'
+    ],
+    'opened' => [
+        'subject' => 'Wiadomość otwarta',
+        'desc' => 'Wiadomość została otwarta przez odbiorcę, poniżej dane dotyczące tej wiadomości:'
+    ],
+    'perm_failure' => [
+        'subject' => 'Błąd dostarczania',
+        'desc' => 'Wiadomość napotkała błąd dostarczania (próby dostarczenia nie będą ponawiane), poniżej dane dotyczące tej wiadomości:'
+    ],
+    'spam' => [
+        'subject' => 'Zgłoszenie spamu',
+        'desc' => 'Niestety, ktoś zgłosił Twoją wiadomość jako spam, poniżej dane dotyczące tej wiadomości:'
+    ],
+    'temp_failure' => [
+        'subject' => 'Tymczasowy błąd dostarczania',
+        'desc' => 'Wiadomość napotkała tymczasowy błąd dostarczania (próby dostarczenia będą ponawiane), poniżej dane dotyczące tej wiadomości:'
+    ],
+    'unsubscribe' => [
+        'subject' => 'Wycofanie subskrypcji',
+        'desc' => 'Niestety, ktoś postanowił wycofać subskrypcję Twojej listy mailingowej, poniżej dane dotyczące tej wiadomości:'
+    ],
+    'fields' => [
+        'recipient' => 'Odbiorca',
+        'msg' => 'Dane wiadomości',
+        'msg_to' => 'Do',
+        'msg_id' => 'ID wiadomości',
+        'msg_from' => 'Od',
+        'msg_subject' => 'Temat',
+        'tags' => 'Etykiety',
+        'variables' => 'Zmienne'
+    ]
+];
