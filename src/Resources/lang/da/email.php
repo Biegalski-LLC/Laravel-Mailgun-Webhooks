dc768d1c23583f662bf187c916216aebff40d869
Tobias Jensen <bluebazze@gmail.com>
2023-07-24 10:59:09 +0200

    Added Danish translations

---
+++ src/Resources/lang/da/email.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'greeting' => 'Hej,',
+    'clicks' => [
+        'subject' => 'Link Klikket',
+        'desc' => 'Modtageren klikkede på et link i din e-mail, data om denne e-mail nedenfor:'
+    ],
+    'delivered' => [
+        'subject' => 'Besked Leveret',
+        'desc' => 'En e-mail blev leveret til modtageren, data om denne e-mail levering nedenfor:'
+    ],
+    'opened' => [
+        'subject' => 'Besked Åbnet',
+        'desc' => 'En e-mail blev åbnet af modtageren, data om denne e-mail levering nedenfor:'
+    ],
+    'perm_failure' => [
+        'subject' => 'Permanent Fejl ved Forsendelse',
+        'desc' => 'En e-mail fejlede permanent i levering til modtageren, data om denne e-mail levering nedenfor:'
+    ],
+    'spam' => [
+        'subject' => 'Spam Rapport',
+        'desc' => 'Desværre har nogen rapporteret dine e-mails som spam, data om denne e-mail levering nedenfor:'
+    ],
+    'temp_failure' => [
+        'subject' => 'Midlertidig Fejl ved Forsendelse',
+        'desc' => 'En e-mail fejlede midlertidigt i levering til modtageren, data om denne e-mail levering nedenfor:'
+    ],
+    'unsubscribe' => [
+        'subject' => 'Afmeldt',
+        'desc' => 'Desværre har nogen afmeldt sig fra din mailingliste, data om denne e-mail levering nedenfor:'
+    ],
+    'fields' => [
+        'recipient' => 'Modtager',
+        'msg' => 'Besked Data',
+        'msg_to' => 'Til',
+        'msg_id' => 'Besked ID',
+        'msg_from' => 'Fra',
+        'msg_subject' => 'Emne',
+        'tags' => 'Tags',
+        'variables' => 'Variable'
+    ]
+];
